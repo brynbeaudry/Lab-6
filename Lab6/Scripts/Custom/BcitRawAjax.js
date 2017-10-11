5682f83907ca57b2e8c66909f42264704486cd01
Bryn Beaudry <babeaudry@gmail.com>
2017-10-10 19:48:41 -0700

    Products get loaded

---
+++ Lab6/Scripts/Custom/BcitRawAjax.js
@@ -0,0 +1,20 @@
+﻿/// <reference path="../jquery-1.6.2-vsdoc.js" />
+
+
+var xhr = new XMLHttpRequest();
+
+function ShowResult() {
+    if (xhr.readyState == 4 && xhr.status == 200) {
+        var timeDiv = document.getElementById("timeDisplayRaw");
+        timeDiv.innerHTML = xhr.responseText;
+    }
+}
+
+
+function getServerTime() {
+    if (xhr) {
+        xhr.open("GET", "/SimpleAjax/ServerTimeAsString");
+        xhr.onreadystatechange = ShowResult;        
+        xhr.send();
+    }
+}
---
+++ Lab6/Scripts/Custom/ProdSearchScripts.js
@@ -0,0 +1,62 @@
+﻿// <reference path="jquery-1.5.1.js" />
+// <reference path="jquery-ui-1.8.11.js" />
+// <reference path="jQuery.tmpl.js" />
+
+
+var loadProducts = function () {
+    var form = $('searchForm')
+    console.log("Form Action", form.attr("action"));
+    console.log("Form Data", form.serialize());
+
+    $.ajax({
+        url: "ProductSearch",
+        type: "GET",
+        data: form.serialize(),
+        beforeSend: function () {
+            $("#searchresults").empty();
+            $("#ajax-loader").show();
+        },
+        complete: function () {
+            $("#ajax-loader").hide();
+        },
+        error: searchFailed,
+        success: function (data) {
+            //$("#productTemplate").tmpl(data).appendTo("#product-list");
+            //console.log(data);
+            $("#searchresults").html(data)
+        }
+    });
+}
+
+
+// short version
+$(function () {
+
+
+    //$("#productSearch").submit(function (event) {
+    //    event.preventDefault();
+    //    var form = $(this);
+    //    $.getJSON(form.attr("action"), form.serialize(), function (data) {
+    //        $("#product-list").empty();
+    //        $("#productTemplate").tmpl(data).appendTo("#product-list");
+    //    });
+    //});
+
+    loadProducts();
+
+    $("#queryInput").on('keyup', function (event) {
+        loadProducts()
+    });
+
+    $("input[data-autocomplete-source]").each(function () {
+        var target = $(this);
+        target.autocomplete({ source: target.attr("data-autocomplete-source") });
+    });
+});
+
+function searchFailed() {
+    $("#searchresults").html("<p style='color:red'>Search failed!</p>");
+}
+
+
+
